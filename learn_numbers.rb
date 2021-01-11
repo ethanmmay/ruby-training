4cc9be3550323189931fd73acaa56657aa596e3d
Ethan May <ethaniel.may@gmail.com>
2021-01-11 13:49:18 -0700

    Manipulating Numbers

---
+++ learn_numbers.rb
@@ -0,0 +1,14 @@
+puts 5.53572390
+puts 5.53572390 - 0.4320525
+puts 10 % 3
+num = 20
+puts num % 3
+puts ("lucky num " + num.to_s)
+puts num.abs()
+puts num -= 0.1
+puts num.round()
+
+puts Math.sqrt(49)
+puts Math.log(1)
+puts 10 / 7
+puts 10 / 7.0
