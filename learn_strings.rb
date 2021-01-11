4c4a0d18e8beb0d83a0053397aea132ffaf8bc6b
Ethan May <ethaniel.may@gmail.com>
2021-01-11 13:43:23 -0700

    Manipulating Strings

---
+++ learn_strings.rb
@@ -0,0 +1,13 @@
+puts "Learn \"Ruby\""
+
+phrase = "Learn Ruby"
+puts phrase
+puts phrase.upcase()
+phrase = "    Learn Ruby Fast    "
+puts phrase.strip().length()
+puts phrase.include? "Ruby"
+phrase = phrase.strip()
+puts phrase[6]
+puts phrase[6, 4]
+puts phrase.index("Fast")
+puts "Phrase".upcase()
