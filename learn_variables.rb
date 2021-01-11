8334a33bb97ee6110a35b4df88164e877009a365
Ethan May <ethaniel.may@gmail.com>
2021-01-10 18:31:14 -0700

    Re-writing Variables

--- learn_variables.rb
+++ learn_variables.rb
@@ -2,5 +2,6 @@ character_name = "John"
 character_age = "35"
 puts ("There once was a man named " + character_name)
 puts ("He was " + character_age + " years old.")
+character_name = "Tom"
 puts ("He really liked the name " + character_name)
 puts ("But didn't like being " + character_age)
