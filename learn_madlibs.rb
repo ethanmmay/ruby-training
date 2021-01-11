9388185d83a87a3ef9635b6464fb5b1d20d26c66
Ethan May <ethaniel.may@gmail.com>
2021-01-11 14:16:18 -0700

    Madlibs

---
+++ learn_madlibs.rb
@@ -0,0 +1,10 @@
+puts "Enter a color: "
+color = gets.chomp
+puts "Enter a plural noun: "
+noun = gets.chomp
+puts "Enter a person: "
+person = gets.chomp
+
+puts ("Roses are " + color)
+puts (noun + " are blue")
+puts ("I love " + person)
