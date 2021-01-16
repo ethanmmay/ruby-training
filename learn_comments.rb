9692b6b93a8841b4a4a7eae41aeaab9dda9acf32
Ethan May <ethaniel.may@gmail.com>
2021-01-15 17:04:22 -0700

    Comments

---
+++ learn_comments.rb
@@ -0,0 +1,22 @@
+# random text
+
+puts "Comments are fun"
+
+# def pow(base_num, pow_num)
+#     result = 1
+#     pow_num.times do |index|
+#         result = result * base_num
+#     end
+#     return result
+# end
+
+# puts pow(7, 2)
+
+puts "I used comments to save some code in this file :) :) :) :) :)"
+
+=begin
+    how to correctly do
+    multiline comments
+    but looks a little
+    harsh on the eyes
+=end
