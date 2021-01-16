6d52aec2e294f3053cb54c801d412a8ed642f67e
Ethan May <ethaniel.may@gmail.com>
2021-01-15 17:12:50 -0700

    Reading Files

---
+++ learn_files.rb
@@ -0,0 +1,13 @@
+
+File.open("testfile.txt", "r") do |file|
+
+    # puts file.readline()
+    # puts file.readchar()
+    for line in file.readlines()
+        puts line
+    end
+    puts file.read().include? "text"
+
+
+
+end
