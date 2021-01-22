5f2ffbe15dc8f3fa2d951536e6647f46dbb70337
Ethan May <ethaniel.may@gmail.com>
2021-01-21 17:34:31 -0700

    Inheritance Base File

---
+++ learn_inheritance.rb
@@ -0,0 +1,23 @@
+class Chef
+    def makeChicken
+        puts "The chef makes chicken"
+    end
+    def makeSalad
+        puts "The chef makes salad"
+    end
+    def makeSpecial
+        puts "The chef makes The Ultimate Sub"
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
